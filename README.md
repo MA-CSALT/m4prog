105f6447928863e50d4bdde8bce545bb02baee11
ruiter <38767@ma-web.nl>
2025-05-23 10:08:35 +0200

    Bewegende Speler

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ beginner code aangepast zodat het werkt. 3 begin berichten, als je op spatie dru
 
 ![animatie](gifs/ScoreEnMunten.gif)
 [code](m4prog/Assets/scripts/PlayerScore.cs)
+
+# Bewegende Speler
+
+gevordende code gebruikt en naar mijn eigen code van vorige dingen gekeken om het te maken.
+
+![animatie](gifs/BewegendeSpeler.gif)
+[code](m4prog/Assets/scripts/PlayerControl.cs)
---
+++ m4prog/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 500f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Debug.Log("Speler klaar!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            Debug.Log("Springen!");
+        }
+    }
+}
